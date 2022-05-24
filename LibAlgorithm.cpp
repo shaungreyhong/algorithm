05aaf5f5cf443a428f1507b1c468cfb63c559b27
Shaun Grey Hong <hong.sukwoo@epfl.ch>
2022-05-24 19:43:11 +0200

    20220524

--- LibAlgorithm.cpp
+++ LibAlgorithm.cpp
@@ -1,36 +1,109 @@
+// C++ program to implement
+// the above approach
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main() {
-    char op;
-    float num1, num2;
+// Class calculator
+class Calculator {
+float a, b;
+public:
 
-    cout << "Enter operator: +, -, *, /: ";
-    cin >> op;
+	// Function to take input
+	// from user
+	void result()
+	{
+		cout << "Enter First Number: ";
+		cin >> a;
+		cout << "Enter Second Number: ";
+		cin >> b;
+	}
 
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
+	// Function to add two numbers
+	float add()
+	{
+		return a + b;
+	}
 
-    switch(op) {
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
-            break;
+	// Function to subtract two numbers
+	float sub()
+	{
+		return a - b;
+	}
 
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
-            break;
+	// Function to multiply two numbers
+	float mul()
+	{
+		return a * b;
+	}
 
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
+	// Function to divide two numbers
+	float div()
+	{
+		if (b == 0)
+		{
+			cout << "Division By Zero" <<
+					endl;
+			return INFINITY;
+		}
+		else
+		{
+			return a / b;
+		}
+	}
+};
 
-        case '/':
-            cout << num1 << " / " << num2 << " = " << num1 / num2;
-            break;
-
-        default:
-            break;
-    }
-
-    return 0;
+// Driver code
+int main()
+{
+	int ch;
+	Calculator c;
+	cout << "Enter 1 to Add 2 Numbers" <<
+			"\nEnter 2 to Subtract 2 Numbers" <<
+			"\nEnter 3 to Multiply 2 Numbers" <<
+			"\nEnter 4 to Divide 2 Numbers" <<
+			"\nEnter 0 To Exit";
+		
+	do
+	{
+		cout << "\nEnter Choice: ";
+		cin >> ch;
+		switch (ch)
+		{
+		case 1:
+			
+			// result function invoked
+			c.result();	
+			
+			// add function to calculate sum
+			cout << "Result: " <<
+					c.add() << endl;
+			break;
+		case 2:
+			
+			// sub function to calculate
+			// difference
+			c.result();
+			cout << "Result: " <<
+					c.sub() << endl;
+			break;
+		case 3:
+			c.result();
+			
+			// mul function to calculate product
+			cout << "Result: " <<
+					c.mul() << endl;
+			break;
+		case 4:
+			c.result();
+			
+			// div function to calculate division
+			cout << "Result: " <<
+					c.div() << endl;
+			break;
+		}
+		
+	} while (ch >= 1 && ch <= 4);
+	
+	return 0;
 }
